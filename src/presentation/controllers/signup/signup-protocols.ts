0f07e709b6b030356408c8b0f0a1dada1a4bc6cd
Allan Egidio <allan.egidio@outlook.com>
2021-02-23 18:48:18 -0300

    refactor: change folder name

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
-export * from '../../../domain/usercases/add-acount'
+export * from '../../../domain/usecases/add-acount'
 export * from '../../../domain/models/account'

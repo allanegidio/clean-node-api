fdd8c2ff06f0574f384925282926d86f18b0e4f0
Allan Egidio <allan.egidio@outlook.com>
2021-02-23 19:49:42 -0300

    refactor: move dbAddAcount creation to a factory helper method

--- src/data/usecases/add-acount/db-add-account.spec.ts
+++ src/data/usecases/add-acount/db-add-account.spec.ts
@@ -1,17 +1,32 @@
 import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-acount'
 
+interface DbAddAccountTypes {
+  dbAddAccount: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const dbAddAccountFactory = (): DbAddAccountTypes => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  const encrypterStub = new EncrypterStub()
+  const dbAddAccount = new DbAddAccount(encrypterStub)
+
+  return {
+    encrypterStub,
+    dbAddAccount
+  }
+}
+
 describe('DbAddAcount UseCase', () => {
   test('Should call Ecrypter with correct password', async () => {
     // Arrange
-    class EncrypterStub implements Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => resolve('hashed_password'))
-      }
-    }
 
-    const encrypterStub = new EncrypterStub()
-    const dbAccount = new DbAddAccount(encrypterStub)
+    const { dbAddAccount, encrypterStub } = dbAddAccountFactory()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
@@ -20,7 +35,7 @@ describe('DbAddAcount UseCase', () => {
     }
 
     // Act
-    await dbAccount.add(accountData)
+    await dbAddAccount.add(accountData)
 
     // Asserts
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')

a38ae7fec93f8a4f300e0312ef420d243f204f9a
Allan Egidio <allan.egidio@outlook.com>
2021-02-25 15:11:44 -0300

    chore: add jest-mongodb to run mongodb on memory during the tests

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,11 +1,14 @@
 module.exports = {
+  mongodbMemoryServer: {
+    version: 'latest'
+  },
   mongodbMemoryServerOptions: {
-    binary: {
-      version: '4.0.3',
-      skipMD5: true
-    },
     instance: {
       dbName: 'jest'
+    },
+    binary: {
+      version: '4.4.4',
+      skipMD5: true
     },
     autoStart: false
   }
--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,10 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
-  // testEnvironment: 'node',
+  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
+  watchPathIgnorePatterns: ['globalConfig']
 }

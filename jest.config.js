fafd16c389f56a94abb27b26520a960add3f9d03
Allan Egidio <allan.egidio@outlook.com>
2021-02-26 20:01:12 -0300

    chore: add root path to jes.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {

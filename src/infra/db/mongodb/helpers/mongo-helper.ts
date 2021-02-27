873b6d555653dc62cfe017d97a28987492e78cff
Allan Egidio <allan.egidio@outlook.com>
2021-02-26 21:02:59 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,8 +4,10 @@ export const MongoHelper = {
 
   // You can't cast from a custom to a primitive without 'erasing' the type first. unknown erases the type checking.
   client: null as unknown as MongoClient,
+  uri: null as unknown as string,
 
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -14,9 +16,15 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null as unknown as MongoClient
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    // if (!this.client || !this.client.isConnected()) -> Old javascript xD
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
+
     return this.client.db().collection(name)
   },
 

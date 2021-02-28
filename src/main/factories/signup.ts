1da5c2031fc065b4175cfd17a9669953edfe6a00
Allan Egidio <allan.egidio@outlook.com>
2021-02-27 21:53:03 -0300

    feat: ensure LogMongoRepository save log error on server error

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-acount/db-add-acount'
 import { BCrypterAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -12,6 +13,7 @@ export const signUpControllerFactory = (): Controller => {
   const bcrypterAdapter = new BCrypterAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(bcrypterAdapter, accountMongoRepository)
+  const logMongoRepository = new LogMongoRepository()
   const signUpController = new SignUpController(emailValidatorAdapter, addAccount)
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
